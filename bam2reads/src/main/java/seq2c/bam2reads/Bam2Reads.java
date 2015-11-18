09e4118f7f9406de1b69d3ff1551e6163108c342
mzueva <mary_zu@rambler.ru>
2015-11-18 19:21:06 +0300

    bam2reads java implementation

--- bam2reads/src/main/java/seq2c/bam2reads/Bam2Reads.java
+++ bam2reads/src/main/java/seq2c/bam2reads/Bam2Reads.java
@@ -21,30 +21,33 @@ public class Bam2Reads {
      * using samtools and gets a number of aligned records from index information.
      * Number of aligned records for each file is recorded into output file,
      * Format of output file:
-     *      one line per bam file
-     *      each line: "sample name" \t "number of aligned records"
+     * one line per bam file
+     * each line: "sample name" \t "number of aligned records"
      *
-     * @param fileIn    contains list of sample names and bam files to process,
-     *                  each line in file represent one file
-     * @param fileOut   output for results
+     * @param fileIn  contains list of sample names and bam files to process,
+     *                each line in file represent one file
+     * @param fileOut output for results
      */
 
     public static void printStatsToFile(String fileIn, String fileOut) {
         Map<String, String> files = parseFile(fileIn);
-        try(FileWriter writer = new FileWriter(fileOut)) {
+        try (FileWriter writer = new FileWriter(fileOut)) {
             for (Map.Entry<String, String> entry : files.entrySet()) {
                 writer.write(entry.getKey() + "\t");
-                File bamFile = new File(entry.getValue());
-                SamReader sam = SamReaderFactory.makeDefault().open(bamFile);
-                SamReader.Indexing ind = sam.indexing();
-                AbstractBAMFileIndex index = (AbstractBAMFileIndex)ind.getIndex();
-                int count = 0;
-                for (int i = 0; i < index.getNumberOfReferences(); i++) {
-                    BAMIndexMetaData meta = index.getMetaData(i);
-                    count += meta.getAlignedRecordCount();
+                try (SamReader sam = SamReaderFactory.makeDefault().open(new File(entry.getValue()))) {
+
+                    SamReader.Indexing ind = sam.indexing();
+                    AbstractBAMFileIndex index = (AbstractBAMFileIndex) ind.getIndex();
+                    int count = 0;
+                    for (int i = 0; i < index.getNumberOfReferences(); i++) {
+                        BAMIndexMetaData meta = index.getMetaData(i);
+                        count += meta.getAlignedRecordCount();
+                    }
+                    writer.write(count + "\n");
+                    writer.flush();
+                } catch (IOException e) {
+                    System.err.println("Cannot read file " + entry.getValue());
                 }
-                writer.write(count + "\n");
-                writer.flush();
             }
         } catch (IOException e) {
             System.err.println("Cannot write to file " + fileOut);
@@ -54,14 +57,15 @@ public class Bam2Reads {
     /**
      * Reads input file line by line and creates a map containing
      * file header/bam file name
-     * @param fileName  contains list of sample names and bam files to process,
-     *                  each line in file represent one file
-     * @return          map with bam file names
+     *
+     * @param fileName contains list of sample names and bam files to process,
+     *                 each line in file represent one file
+     * @return map with bam file names
      */
     private static Map<String, String> parseFile(String fileName) {
         Map<String, String> map = new LinkedHashMap<>();
 
-        try(BufferedReader reader = new BufferedReader(new FileReader(new File(fileName)))) {
+        try (BufferedReader reader = new BufferedReader(new FileReader(new File(fileName)))) {
             String currentLine;
             while ((currentLine = reader.readLine()) != null) {
                 String[] samples = currentLine.split("\\s+");
